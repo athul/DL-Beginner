baf4c97849c700be8d72dee8ee7f247c4fc2e381
Athul Cyriac Ajay <40897573+Athul-CA@users.noreply.github.com>
2018-10-09 20:01:55 +0530

    Add files via upload

---
+++ TensorflowNN.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+sess = tf.InteractiveSession()
+f_mnist=keras.datasets.fashion_mnist
+(train_images,train_labels),(test_images,test_labels)=f_mnist.load_data()
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+model.compile(optimizer=tf.train.AdamOptimizer(), 
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(train_images,train_labels)
+i = 0
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions, test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions,  test_labels)              
+sess.close()
---
+++ sklr.py
@@ -0,0 +1,13 @@
+from sklearn import neighbors, datasets, preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+iris = datasets.load_iris()
+X, y = iris.data[:, :2], iris.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=33)
+scaler = preprocessing.StandardScaler().fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+knn = neighbors.KNeighborsClassifier(n_neighbors=5)
+knn.fit(X_train, y_train)
+y_pred = knn.predict(X_test)
+accuracy_score(y_test, y_pred)
